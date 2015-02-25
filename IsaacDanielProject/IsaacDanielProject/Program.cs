0bc7201025d60a0387db82e50344b6296f2b83ff
isaacjensen - Isaac <isaac.jensen@outlook.com>
2015-02-24 20:10:01 -0800

    First Commit - Isaac

---
+++ IsaacDanielProject/IsaacDanielProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsaacDanielProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int a = 5;
+            int b = 10;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+
+        }
+    }
+}
